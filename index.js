47a1845d31f27e8e902efac5bc32099359579af2
Bhfreagra <eberdellima@outlook.com>
2020-01-18 02:46:41 +0100

    add: cors policy

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+
 const redis = require('redis')
 const client = redis.createClient()
 const { promisify } = require('util')
 const getAsync = promisify(client.get).bind(client)
 
 app.get('/jobs', async(req, res) => {
-  const data = await getAsync('github')
+  const result = await getAsync('github')
+  const data = JSON.parse(result)
   return res.send({data})
 })
 
